09642d2fe0391dca3f32d2abafb27addbfdb1298
Vinicius Menezes <vicmen22@gmail.com>
2021-12-22 15:05:48 -0400

    Componente Botão #03

--- src/components/Botao.svelte
+++ src/components/Botao.svelte
@@ -2,15 +2,19 @@
     export let texto: string;
     export let triplo: boolean = false;
     export let duplo: boolean = false;
+    export let operacao: boolean = false;
+    export let destaque: boolean = false;
 </script>
 
-<button class="botao" class:triplo class:duplo>
+<button class="botao" class:triplo class:duplo class:operacao class:destaque>
     {texto}
 </button>
 
 <style>
     .botao {
         flex-basis: 25%;
+        margin: 0;
+        border: 1px solid var(--cor-fundo);
     }
 
     .triplo {
@@ -19,5 +23,15 @@
 
     .duplo {
         flex-basis: 50%;
+    }
+
+    .operacao {
+        background-color: orange;
+        color: white;
+    }
+
+    .destaque {
+        background-color: red;
+        color: white;
     }
 </style>
--- src/components/Calculadora.svelte
+++ src/components/Calculadora.svelte
@@ -5,31 +5,31 @@
 
 <div class="calculadora">
     <Linha>
-        <Botao texto="AC" triplo />
-        <Botao texto="/" />
+        <Botao texto="AC" triplo destaque />
+        <Botao texto="/" operacao />
     </Linha>
     <Linha>
         <Botao texto="7" />
         <Botao texto="8" />
         <Botao texto="9" />
-        <Botao texto="*" />
+        <Botao texto="*" operacao />
     </Linha>
     <Linha>
         <Botao texto="4" />
         <Botao texto="5" />
         <Botao texto="6" />
-        <Botao texto="+" />
+        <Botao texto="+" operacao />
     </Linha>
     <Linha>
         <Botao texto="1" />
         <Botao texto="2" />
         <Botao texto="3" />
-        <Botao texto="-" />
+        <Botao texto="-" operacao />
     </Linha>
     <Linha>
         <Botao texto="0" duplo />
         <Botao texto="," />
-        <Botao texto="=" />
+        <Botao texto="=" destaque />
     </Linha>
 </div>
 
@@ -40,5 +40,7 @@
         width: 150px;
         display: flex;
         flex-direction: column;
+        padding: 10px;
+        border-radius: 10px;
     }
 </style>
