dc727c001ef90c076ea5452fc727c0f410864298
Vinicius Menezes <vicmen22@gmail.com>
2021-12-22 15:12:30 -0400

    Componente Tela #01

--- src/components/Calculadora.svelte
+++ src/components/Calculadora.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
     import Botao from "./Botao.svelte";
     import Linha from "./Linha.svelte";
+    import Tela from "./Tela.svelte";
 </script>
 
 <div class="calculadora">
+    <Tela valor="123" />
     <Linha>
         <Botao texto="AC" triplo destaque />
         <Botao texto="/" operacao />
---
+++ src/components/Tela.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    export let valor: string;
+</script>
+
+<div class="tela">
+    <span>{valor}</span>
+</div>
+
+<style>
+    .tela {
+        display: flex;
+        background-color: aliceblue;
+        flex: 1;
+        justify-content: flex-end;
+        align-items: center;
+        padding: 0px 5px;
+        overflow: hidden;
+        font-size: 1.7rem;
+        margin: 2px 1px;
+        border-top-right-radius: 5px;
+        border-top-left-radius: 5px;
+    }
+</style>
